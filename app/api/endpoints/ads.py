30bfb669a5cce7ec0fe5a30c2c30807e4d7c405a
nyon <nilanoyon371@gmail.com>
2026-07-16 01:00:04 +0600

    disable ads

--- app/api/endpoints/ads.py
+++ app/api/endpoints/ads.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 router = APIRouter()
 
 ADS_CONFIG = {
-    "enabled": True,
+    "enabled": False,
     "adsterra": {
         "smartlink": "https://www.effectivecpmnetwork.com/yybezk25q9?key=6b4fbb4ef5cf23053ffe1032c312ae3d",
     },
