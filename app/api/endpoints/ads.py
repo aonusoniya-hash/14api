05e4648f03f914b584f5680ce7c8d5922e5d7871
nyon <nilanoyon371@gmail.com>
2026-07-16 00:54:50 +0600

    remove unity

--- app/api/endpoints/ads.py
+++ app/api/endpoints/ads.py
@@ -4,22 +4,6 @@ router = APIRouter()
 
 ADS_CONFIG = {
     "enabled": True,
-    "interstitialProvider": "adsterra",  # adsterra | unity
-    "rewardedProvider": "adsterra",      # adsterra | unity
-    "unity": {
-        "gameId": "6045785",
-        "testMode": False,
-        "placements": {
-            "interstitial": {
-                "android": "Interstitial_Android",
-                "ios": "Interstitial_iOS",
-            },
-            "rewarded": {
-                "android": "Rewarded_Android",
-                "ios": "Rewarded_iOS",
-            },
-        },
-    },
     "adsterra": {
         "smartlink": "https://www.effectivecpmnetwork.com/yybezk25q9?key=6b4fbb4ef5cf23053ffe1032c312ae3d",
     },
