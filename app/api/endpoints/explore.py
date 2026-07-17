479d386642ea232ecf9e62435b838282ce9db1d5
nyon <nilanoyon371@gmail.com>
2026-07-18 04:28:06 +0600

    2

--- app/api/endpoints/explore.py
+++ app/api/endpoints/explore.py
@@ -1091,7 +1091,7 @@ EXPLORE_SOURCES = [
         hasCategories=True,
         searchUrlTemplate="https://animeidhentai.com/search?q={query}",
         sourceId="animeidhentai",
-        disable=False,
+        disable=True,
         pageSize=28,
     ),
     ExploreSourceResponse(
